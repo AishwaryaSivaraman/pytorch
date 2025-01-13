116027b92510ba76d078431e548e0f3ed3ec27cd
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-01-13 20:52:43 +0000

    Suport generators
    ghstack-source-id: 4d9975094b3e5a460da49c8b429de653b8acd8ab
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141055

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -408,6 +408,9 @@ enable_cpp_guard_manager = True
 # Enable tracing through contextlib.contextmanager
 enable_trace_contextlib = True
 
+# Changes dynamo behavior on generator functions
+enable_faithful_generator_behavior = False
+
 # Inline inbuilt nn modules
 inline_inbuilt_nn_modules = not is_fbcode()
 
