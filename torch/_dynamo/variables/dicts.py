2e23ae2cd409acc6652865d7d9360861c00c9ac2
Animesh Jain <anijain@umich.edu>
2025-01-13 16:40:49 -0800

    [dynamo] Use polyfill to implement operator.eq
    ghstack-source-id: eff274650403ca0c1759aaaaf33ceb0ee632c574
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144485

--- torch/_dynamo/variables/dicts.py
+++ torch/_dynamo/variables/dicts.py
@@ -746,7 +746,9 @@ class DictKeySetVariable(SetVariable):
         return dict_keys
 
     def as_python_constant(self):
-        unimplemented("DictKeySetVariable.as_python_constant")
+        return dict.fromkeys(
+            {k.vt.as_python_constant() for k in self.set_items}, None
+        ).keys()
 
     def call_method(
         self,
